10268d923e5c1e1cc7c4c8dd0dd67e2111b87d96
xmzhang <254700837@qq.com>
2021-02-22 17:46:04 +0800

    add百变大咖秀

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -3,3 +3,6 @@
 
 # 华硕-爱奇艺
 1 0 22-28 2 * node /scripts/jd_asus_iqiyi.js >> /scripts/logs/jd_asus_iqiyi.log 2>&1
+
+# 百变大咖秀
+10 10,11 * * 2-5 node /scripts/jd_entertainment.js >> /scripts/logs/jd_entertainment.log 2>&1
