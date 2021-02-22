e145e8bd87918b5edad41e84ef26aa3a5ff3a69f
xmzhang <254700837@qq.com>
2021-02-22 17:50:09 +0800

    粉丝互动

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -6,3 +6,6 @@
 
 # 百变大咖秀
 10 10,11 * * 2-5 node /scripts/jd_entertainment.js >> /scripts/logs/jd_entertainment.log 2>&1
+
+# 粉丝互动
+3 10 * * * node/scripts/jd_fanslove.js >> /scripts/logs/jd_fanslove.log 2>&1
