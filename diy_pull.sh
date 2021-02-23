fbf79fc7376e10cbe2dfdafc9628dc3c404f873d
xmzhang <254700837@qq.com>
2021-02-23 18:30:49 +0800

    update

--- diy_pull.sh
+++ diy_pull.sh
@@ -372,8 +372,8 @@ if [[ ${ExitStatusShell} -eq 0 ]]; then
   [ -d ${ScriptsDir}/.git ] && Git_PullScripts || Git_CloneScripts
   [ -d ${Scripts2Dir}/.git ] && Git_PullScripts2 || Git_CloneScripts2
   cp -f ${Scripts2Dir}/jd_*.js ${ScriptsDir}
-  cp -rf ${Scripts2Dir}/diy_pull.sh ${ConfigDir}
-  cp -rf ${Scripts2Dir}/config.sh.sample ${ConfigDir}/config.sh.sample
+  cp -f ${Scripts2Dir}/diy_pull.sh ${ConfigDir}
+  cp -f ${Scripts2Dir}/config.sh.sample ${ConfigDir}/config.sh.sample
 fi
 
 ## 执行各函数
