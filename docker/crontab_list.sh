7140bedf326615e50a6fadb48ddbd9b234574238
xmzhang <254700837@qq.com>
2021-02-25 11:54:40 +0800

    直播间红包雨

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -6,3 +6,6 @@
 
 # 百变大咖秀
 10 10,11 * * * node /scripts/jd_entertainment.js >> /scripts/logs/jd_entertainment.log 2>&1
+
+# 直播间红包雨
+0 0,9,11,13,15,17,19,20,21,23 * * * node /scripts/jd_live_redrain.js >> /scripts/logs/jd_live_redrain.log 2>&1
--- jd_live_redrain_nian.js
+++ jd_live_redrain_nian.js
@@ -44,17 +44,17 @@ if ($.isNode()) {
 }
 const JD_API_HOST = 'https://api.m.jd.com/api';
 let ids = {
-  '0': 'RRA3S6TRRbnNNuGN43oHMA5okbcXmRY',
-  '9': 'RRA3S6TRRbnNNuGN43oHMA5okbcXmRY',
-  '11': 'RRA3S6TRRbnNNuGN43oHMA5okbcXmRY',
-  '13': 'RRA3S6TRRbnNNuGN43oHMA5okbcXmRY',
-  '15': 'RRA3S6TRRbnNNuGN43oHMA5okbcXmRY',
-  '17': 'RRA3S6TRRbnNNuGN43oHMA5okbcXmRY',
-  '19': 'RRA3S6TRRbnNNuGN43oHMA5okbcXmRY',
-  '20': 'RRA3S6TRRbnNNuGN43oHMA5okbcXmRY',
-  '21': 'RRA3S6TRRbnNNuGN43oHMA5okbcXmRY',
-  '22': 'RRA3S6TRRbnNNuGN43oHMA5okbcXmRY',
-  '23': 'RRA3S6TRRbnNNuGN43oHMA5okbcXmRY'
+  '0': 'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
+  '9': 'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
+  '11': 'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
+  '13': 'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
+  '15': 'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
+  '17': 'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
+  '19': 'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
+  '20': 'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
+  '21': 'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
+  '22': 'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX',
+  '23': 'RRA3u4B8NPFuMt9tSZGagiEsWjyjdBX'
 }
 !(async () => {
   if (!cookiesArr[0]) {
