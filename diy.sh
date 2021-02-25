bf72b138c5a82f5fdb4af214de42ac9869aac23a
xmzhang <254700837@qq.com>
2021-02-25 14:57:38 +0800

    update

--- diy.sh
+++ diy.sh
@@ -1,6 +1,6 @@
+SCRIPTS_DIR="$JD_DIR/scripts"
 ## 净化脚本内置助力
 function clearCode {
-    SCRIPTS_DIR="$JD_DIR/scripts"
     for file in $SCRIPTS_DIR/*.js; do
         LINES=($(sed -n '/将采纳本脚本自带的助力码/=' $file))
         if [ -n "$LINES" ] ; then
@@ -18,11 +18,12 @@ function clearCode {
 }
 
 function updateCodeUrl {
-    sed -i "s|http://jd.turinglabs.net/api/v2/jd/farm/read|http://106.13.212.194/jd/code/read/farm|"
-    sed -i "s|http://jd.turinglabs.net/api/v2/jd/ddfactory/read|http://106.13.212.194/jd/code/read/ddfactory|"
-    sed -i "s|http://jd.turinglabs.net/api/v2/jd/jxfactory/read|http://106.13.212.194/jd/code/read/jxfactory|"
-    sed -i "s|http://jd.turinglabs.net/api/v2/jd/pet/read|http://106.13.212.194/jd/code/read/pet|"
-    sed -i "s|http://jd.turinglabs.net/api/v2/jd/sgmh/read|http://106.13.212.194/jd/code/read/sgmh|"
+    sed -i "s|http://jd.turinglabs.net/api/v2/jd/farm/read|http://106.13.212.194/jd/code/read/farm|" "$SCRIPTS_DIR/jd_fruit.js"
+    sed -i "s|http://jd.turinglabs.net/api/v2/jd/ddfactory/read|http://106.13.212.194/jd/code/read/ddfactory|" "$SCRIPTS_DIR/jd_jdfactory"
+    sed -i "s|http://jd.turinglabs.net/api/v2/jd/jxfactory/read|http://106.13.212.194/jd/code/read/jxfactory|" "$SCRIPTS_DIR/jd_dreamFactory.js"
+    sed -i "s|http://jd.turinglabs.net/api/v2/jd/pet/read|http://106.13.212.194/jd/code/read/pet|" "$SCRIPTS_DIR/jd_pet.js"
+    sed -i "s|http://jd.turinglabs.net/api/v2/jd/sgmh/read|http://106.13.212.194/jd/code/read/sgmh|" "$SCRIPTS_DIR/jd_sgmh.js"
+    sed -i "s|http://jd.turinglabs.net/api/v2/jd/bean/read|http://106.13.212.194/jd/code/read/bean|" "$SCRIPTS_DIR/jd_plantBean.js"
     echo "助力池链接替换完成!"
 }
 
