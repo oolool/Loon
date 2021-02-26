d0191579c5a6cef22b9a88864e56c0fbc221126c
xmzhang <254700837@qq.com>
2021-02-26 20:44:55 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -8,4 +8,7 @@
 10 10,11 * * * node /scripts/jd_entertainment.js >> /scripts/logs/jd_entertainment.log 2>&1
 
 # 直播间红包雨
-0 0,9,11,13,15,17,19,20,21,23 * * * node /scripts/jd_live_redrain_nian.js >> /scripts/logs/jd_live_redrain_nian.log 2>&1
+# 0 0,9,11,13,15,17,19,20,21,23 * * * node /scripts/jd_live_redrain_nian.js >> /scripts/logs/jd_live_redrain_nian.log 2>&1
+
+# 女装盲盒
+0 8 1-8/1,27,28 2,3 * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
